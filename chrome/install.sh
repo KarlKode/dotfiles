9b69cf2d2c8d9f4d981cfb7426b1114da8694061
Marc Gähwiler <marc@marcg.ch>
2015-09-24 17:15:16 +0200

    Unneeded installers...

--- chrome/install.sh
+++ chrome/install.sh
@@ -2,7 +2,12 @@
 # Fix chrome web inspector fonts to be readable.
 # http://blog.dotsmart.net/2011/09/30/change-font-size-in-chrome-devtools/
 
+local chrome_dir="$HOME/Library/Application Support/Google/Chrome"
+
+# Only install on osx and if chrome seems to be installed
+( test "$(uname)" != "Darwin" || ! test -e "${chrome_css_dir}" ) && return;
+
 # Use:
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-ln -s -f $DIR/Custom.css $HOME/Library/Application\ Support/Google/Chrome/Default/User\ StyleSheets/Custom.css
+ln -s -f "${DIR}/Custom.css" "${chrome_dir}/Default/User\ StyleSheets/Custom.css"
 
