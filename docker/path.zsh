d03eed38f830470d5242bbf9bbe19b971c923727
Marc Gähwiler <marc@marcg.ch>
2015-10-01 03:10:30 +0200

    Updated prompt

--- docker/path.zsh
+++ docker/path.zsh
@@ -4,7 +4,6 @@ command -v docker-machine > /dev/null 2>&1 || return;
 # Cache docker output command until next reboot
 docker_cache="$HOME/.cache/docker-init"
 if [[ ! -e "$docker_cache" || -e `find "$docker_cache" -mmin +$cache_reboot` ]]; then
-  echo "old"
   docker-machine env dev >| "$docker_cache"
 fi
 
--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,13 +1,16 @@
 # Only execute this file if pyenv is available
 command -v pyenv > /dev/null 2>&1 || return
 
+export PATH="/Users/m/.pyenv/shims:${PATH}"
+export PYENV_SHELL=zsh
 export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
-#function pyenv {
-  eval "$(pyenv init -)"
-  eval "$(pyenv virtualenv-init -)"
-  pyenv virtualenvwrapper_lazy
-#  command pyenv "$@"
-#}
-
 export VIRTUAL_ENV_DISABLE_PROMPT=true
+
+function pyenv() {
+  unset -f pyenv
+  eval "$(pyenv init --no-rehash - zsh)"
+  eval "$(pyenv virtualenv-init -)"
+  pyenv virtualenvwrapper
+  pyenv "$@"
+}
 
