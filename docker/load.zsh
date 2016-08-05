c5f61ad9d3b07819e414802355ca82eca0191baf
Marc Gähwiler <marc@marcg.ch>
2016-08-05 10:53:13 +0200

    Lazy docker loading

--- docker/load.zsh
+++ docker/load.zsh
@@ -4,10 +4,10 @@ command -v docker-machine > /dev/null 2>&1 || return;
 # Cache docker output command until next reboot
 docker_cache="$HOME/.cache/docker-init"
 if [[ ! -e "$docker_cache" || -e `find "$docker_cache" -mmin +$cache_reboot` ]]; then
-  docker-machine env dev >| "$docker_cache"
+  docker-machine env default >| "$docker_cache"
 fi
 
-# Load environment variables for the dev docker machine
+# Load environment variables for the default docker machine
 source "$docker_cache"
 
 unset docker_cache
